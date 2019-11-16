d9c0faa2a9ba644d5d20bae9b2eb41c1d34dbaea
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2019-11-16 10:03:01 +0900

    Routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-heroes',
@@ -10,8 +11,6 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
-
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
 
@@ -23,8 +22,5 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
 }
